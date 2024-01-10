710d0e521e33cf32d94df246d56b6980f7c12c0a
Joscha <j.pracht2000@gmail.com>
2024-01-10 14:53:08 +0100

    Base URL set to localhost

--- codeigniter/app/Config/App.php
+++ codeigniter/app/Config/App.php
@@ -16,7 +16,7 @@ class App extends BaseConfig
      *
      *    http://example.com/
      */
-    public string $baseURL = 'https://team05.wi1cm.uni-trier.de/codeigniter/public/';
+    public string $baseURL = 'https://localhost/public';
 
     /**
      * Allowed Hostnames in the Site URL other than the hostname in the baseURL.
