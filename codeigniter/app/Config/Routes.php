8d931f6d5fe018366735e8bc1f4b23a7be485c49
Gosskie <150435211+Gosskie@users.noreply.github.com>
2024-01-17 16:00:17 +0100

    default controller auf home gesetzt, wenn falsch dann änder ich das zurück!

--- codeigniter/app/Config/Routes.php
+++ codeigniter/app/Config/Routes.php
@@ -5,10 +5,13 @@ use CodeIgniter\Router\RouteCollection;
 /**
  * @var RouteCollection $routes
  */
+$routes->setDefaultController('Home');
 
 $routes->get('/','Gruppennummer::Startseite');
 $routes->get('Spalten','Gruppennummer::Spalten');
 $routes->get('Formular','Gruppennummer::Formular');
 $routes->get('Startseite','Gruppennummer::Startseite');
+$routes->get('test','TestController::Test');
+
 
 
